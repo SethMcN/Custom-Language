344930595c8dcaa426b22b0e1ad42a3fcef0621e
SethMcn <107069935+SethMcN@users.noreply.github.com>
2024-12-22 22:20:58 +0000

    feat: Implement tokenization and update SMPL interpreter for new command handling

--- SMPL_interpreter.c
+++ SMPL_interpreter.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
-int ListComp(){
-    
-    return 1;
+typedef struct
+{
+    char command[256];
+    char brackets[256];
+} TokenResult;
+
+TokenResult tokenize(const char *line)
+{
+    TokenResult result = {"", ""};
+
+    int commandIndex = 0;
+    int bracketIndex = -1;
+    int bracketFound = 0;
+
+    for (int i = 0; i < strlen(line); i++)
+    {
+        if (line[i] == '(')
+        {
+            bracketFound = 1;
+        }
+        else if (line[i] == ')')
+        {
+            bracketFound = 0;
+        }
+
+        if (bracketFound == 1)
+        {
+            result.brackets[bracketIndex++] = line[i];
+        }
+        else
+        {
+            result.command[commandIndex++] = line[i];
+        }
+    }
+
+    result.command[commandIndex - 2] = '\0';
+
+    return result;
 }
-
-
 
 int main()
 {
@@ -14,7 +47,7 @@ int main()
     fptr = fopen("firstCode.smpl", "r");
     if (fptr == NULL)
     {
-        printf("Error! File not found\n");  
+        printf("Error! File not found\n");
         return 0;
     }
 
@@ -22,17 +55,27 @@ int main()
     printf("The content of the file SMPL_interpreter.txt is:\n");
     printf("------------------------------------------------\n");
 
-    while (fgets(line, sizeof(line), fptr))
+    while (fgets(line, sizeof(line), fptr) != NULL)
     {
-        if (line[0] == ' ' || line[0] == '\n'){
+        if (line[0] == ' ' || line[0] == '\n' || (line[0] == 'B' && line[1] == 'T' && line[2] == 'W'))
+        {
             continue;
         }
+        else
+        {
 
-        else{
             printf("%s", line);
-            char *result = strstr(line, "say_my_name");
-  
-            printf("Hello, my name is %s\n", result);
+
+            int lenLine = strlen(line);
+
+            TokenResult result = tokenize(line);
+
+            if (strcmp(result.command, "shout") == 0)
+            {
+                printf("%s\n", result.brackets);
+            }
+        }
     }
+    fclose(fptr);
     return 0;
-}}
+}
